1c4b9bee052af65b41d64d8a18b4ec1f120e30e7
pgr179 <46944311+pgr179@users.noreply.github.com>
2024-10-14 21:04:42 -0400

    Pre-pub updates

--- Affix_phonology.R
+++ Affix_phonology.R
@@ -13,7 +13,8 @@ library(ggeffects)
 library(car)
 library(data.table)
 library(performance)
-library(ggpubr)
+library(patchwork)
+
 
 
 # Read in the data
@@ -84,7 +85,8 @@ p1 <- ggplot(data, aes(x = Segments, group = Type, color = Type)) +
                  data = ~ subset(., Type %in% c("S"))) +
   scale_color_manual(values = c("P" = "cyan2", "S" = "deeppink1"), labels=c('Prefixes', 'Suffixes')) +
   geom_hline(yintercept = 0, color = "grey30") +
-  scale_y_continuous(limits = c(-.36, .36), breaks = c(-0.2, 0, 0.2), labels = c("0.2", "0", "0.2")) +
+  scale_y_continuous(limits = c(-.38, .38), breaks = c(-0.2, 0, 0.2), 
+                     labels = c("0.2", "0", "0.2")) +
   theme(legend.title=element_blank(), axis.title.y=element_blank()) +
   scale_x_continuous(breaks = c(1:8)) +
   labs(x = "Number of segments")
@@ -98,12 +100,13 @@ p2 <- ggplot(data, aes(x = Syllables, group = Type, color = Type)) +
   scale_color_manual(values = c("P" = "cyan2", "S" = "deeppink1"), labels=c('Prefixes', 'Suffixes')) +
   geom_hline(yintercept = 0, color = "grey30") +
   scale_y_continuous(limits = c(-.63, .63), breaks = c(-0.4, 0, 0.4), labels = c("0.4", "0", "0.4")) +
-  theme(legend.title=element_blank()) +
+  theme(legend.position="none") +
   labs(y = "Proportion (within each affix type)", x = "Number of syllables")
 
-tiff("histograms_by_type.tiff", units="in", width=6, height=3, res=300)
-ggarrange(p2, p1, ncol=2, nrow=1, common.legend = TRUE, legend="right")
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/histograms_by_type.tiff", units="in", width=6, height=3, res=300)
+p2 + p1
 dev.off()
+
 
 
 # Relationship of allomorphy to affix type
@@ -145,23 +148,39 @@ colnames(count_data)[1] <- "Syllabicity"
 colnames(count_data)[2] <- "Language"
 count_data$Syllabicity <- relevel(count_data$Syllabicity, "Non-monosyllabic")
 
+summary(data$Language)
+
 
 # Regression model
 
 # Initial model
-m100 <- glmer(Count ~ Syllabicity + (1 + Syllabicity|Language), count_data, family = poisson(link = "sqrt"))
+m100 <- glmer(Count ~ Syllabicity + (1|Language), count_data, family = poisson())
 m100
 
+# Fixed Effects:
+#   (Intercept)  SyllabicityMonosyllabic  
+# 2.9569                   0.4187
+
+# Random effects:
+#  Groups   Name        Std.Dev.
+# Language (Intercept) 0.6847
+
 # Alternative model and comparison
-m101 <- glmer(Count ~ 1 + (1 + Syllabicity|Language), count_data, family = poisson(link = "sqrt"))
+m101 <- glmer(Count ~ 1 + (1|Language), count_data, family = poisson())
 anova(m100, m101)
 # npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)    
-# m101    4 428.75 436.40 -210.38   420.75                         
-# m100    5 418.88 428.44 -204.44   408.88 11.869  1  0.0005708 ***
+# m101    2 541.01 544.83 -268.50   537.01                         
+# m100    3 480.66 486.40 -237.33   474.66 62.345  1  2.883e-15 ***
+
+#χ2(1) = 62.35, p < 0.001 ***
 
 # Get confidence intervals for the model
 ci <- confint(m100)
 ci
+#                         2.5 %     97.5 %
+#  .sig01                 0.5028107 0.9725700
+#(Intercept)              2.6541230 3.2470634
+#SyllabicityMonosyllabic  0.3139582 0.5241309
 
 pp <- profile(m100)
 ggplot(as.data.frame(pp),aes(.focal,.zeta))+
@@ -172,13 +191,12 @@ ggplot(as.data.frame(pp),aes(.focal,.zeta))+
              colour="gray")
 
 # Quick look
-plot(allEffects(m200))
+plot(allEffects(m100))
 
 # Check residuals
 plot(m100)
 rs <- DHARMa::simulateResiduals(m100)
 plot(rs)
-DHARMa::plotResiduals(rs, form = data$Type, quantreg = T)
 
 # Plot random effects
 plot_model(m100, ,type="pred", terms=c("Syllabicity", "Language"), pred.type="re", ci.lvl = NA)
@@ -187,14 +205,10 @@ plot_model(m100, ,type="pred", terms=c("Syllabicity", "Language"), pred.type="re
 preds <- as.data.frame(predictorEffect("Syllabicity", m100))
 
 # Plot predictions of model
-tiff("count_by_monosyllabicity.tiff", units="in", width=3, height=5, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/count_by_monosyllabicity.tiff", units="in", width=3, height=5, res=300)
 ggplot() +
   geom_pointrange(data = preds, aes(x = Syllabicity, y = fit, ymin = lower, ymax = upper)) +
   geom_errorbar(data = preds, aes(x = Syllabicity, y = fit, ymin = lower, ymax = upper), width = .05) +
-  #geom_rug(data = p, sides = "l", position = position_jitter(height = 0.15),
-  # aes(x = Type, y = Segments), alpha = 1/20) +
-  #geom_rug(data = s, sides = "r", position = position_jitter(height = 0.15),
-  # aes(x = Type, y = Segments), alpha = 1/20) +
   scale_y_continuous(limits = c(0, 50)) +
   labs(y = "Number of affixes") +
   scale_x_discrete(labels=c("0" = "Non-monosyllabic", "1" = "Monosyllabic")) +
@@ -203,7 +217,7 @@ ggplot() +
 dev.off()
 
 # Histogram of syllable lengths per language
-tiff("syllable_lengths_by_language.tiff", units="in", width=8, height=8, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/syllable_lengths_by_language.tiff", units="in", width=8, height=8, res=300)
 ggplot(data, aes(x = Syllables)) +
   geom_histogram(aes(fill = Syllabicity), bins = 9) +
   facet_wrap(vars(Language), ncol = 5) +
@@ -238,16 +252,34 @@ m01_ <- lmer(Segments_bc ~ Type + (1 + Type|Language), data, REML = FALSE)
 m02_ <- lmer(Segments_bc ~ 1 + (1 + Type|Language), data, REML = FALSE)
 anova(m01_, m02_)
 # npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)   
-# m02_    5 2475.4 2501.8 -1232.7   2465.4                        
-# m01_    6 2469.1 2500.8 -1228.6   2457.1 8.2705  1   0.004029 **
+# m02_    5 1928.9 1955.3 -959.45   1918.9                        
+# m01_    6 1921.0 1952.7 -954.51   1909.0 9.8885  1   0.001663 **
 
 # REML = TRUE model for reporting
 m01 <- lmer(Segments_bc ~ Type + (1 + Type|Language), data)
 m01
 
+# Fixed Effects:
+#   (Intercept)        TypeS  
+# 0.6365       0.2308
+
+# Random effects:
+#   Groups   Name        Std.Dev. Corr 
+# Language (Intercept) 0.3173        
+# TypeS       0.2552   -0.79
+# Residual             0.4534
+
 # Get confidence intervals for the model
 ci <- confint(m01)
 ci
+
+# 2.5 %     97.5 %
+#   .sig01       0.21969598  0.4507453
+# .sig02      -0.92636898 -0.4788438
+# .sig03       0.15335713  0.3841382
+# .sigma       0.43722236  0.4707338
+# (Intercept)  0.48441988  0.7915335
+# TypeS        0.09577835  0.3745494
 
 pp <- profile(m01)
 ggplot(as.data.frame(pp),aes(.focal,.zeta))+
@@ -261,7 +293,6 @@ ggplot(as.data.frame(pp),aes(.focal,.zeta))+
 plot(m01)
 rs <- DHARMa::simulateResiduals(m01)
 plot(rs)
-DHARMa::plotResiduals(rs, form = data$Type, quantreg = T)
 
 # Quick look
 plot(allEffects(m01))
@@ -292,7 +323,7 @@ p <- data[data$Type == "P",]
 s <- data[data$Type == "S",]
 
 # Plot
-tiff("affix_length_by_type.tiff", units="in", width=3, height=5, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/affix_length_by_type.tiff", units="in", width=3, height=5, res=300)
 ggplot() +
   geom_pointrange(data = preds, aes(x = Type, y = fit_nbc, ymin = lower_nbc, ymax = upper_nbc)) +
   geom_errorbar(data = preds, aes(x = Type, y = fit_nbc, ymin = lower_nbc, ymax = upper_nbc), width = .05) +
@@ -327,23 +358,41 @@ ggplot(data, aes(x = Type, y = Syllables)) +
 # Regression model
 
 # REML = FALSE models for model selection
-m51_ <- lmer(Syllables_bc ~ Type + (0 + Type|Language), data, REML = FALSE)
+m51_ <- lmer(Syllables_bc ~ Type + (1 + Type|Language), data, REML = FALSE)
 m51_
 
 # Alternative model and comparison
-m52_ <- lmer(Syllables_bc ~ 1 + (0 + Type|Language), data, REML = FALSE)
+m52_ <- lmer(Syllables_bc ~ 1 + (1 + Type|Language), data, REML = FALSE)
 anova(m51_, m52_)
-# npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)   
-# m52_    5 2289.0 2314.8 -1139.5   2279.0                        
-# m51_    6 2282.5 2313.5 -1135.3   2270.5 8.4659  1   0.003619 **
+#     npar    AIC    BIC  logLik deviance  Chisq Df Pr(>Chisq)   
+# m52_    5 2569.3 2595.7 -1279.6   2559.3                        
+# m51_    6 2562.0 2593.7 -1275.0   2550.0 9.3203  1   0.002266 **
 
 # REML = TRUE model for reporting
-m51 <- lmer(Syllables_bc ~ Type + (0 + Type|Language), data)
+m51 <- lmer(Syllables_bc ~ Type + (1 + Type|Language), data)
 m51
+
+# Fixed Effects:
+# (Intercept)        TypeS  
+# -0.1597       0.3379
+
+# Random effects:
+#   Groups   Name        Std.Dev. Corr 
+# Language (Intercept) 0.4413        
+# TypeS       0.4026   -0.80
+# Residual             0.5631
 
 # Get confidence intervals for the model
 ci <- confint(m51)
 ci
+
+#                  2.5 %      97.5 %
+# .sig01       0.3075871  0.62582556
+# .sig02      -0.9207960 -0.51550815
+# .sig03       0.2630144  0.58876230
+# .sigma       0.5429430  0.58452772
+# (Intercept) -0.3687001  0.05704907
+# TypeS        0.1350062  0.54705419
 
 pp <- profile(m51)
 ggplot(as.data.frame(pp),aes(.focal,.zeta))+
@@ -357,7 +406,6 @@ ggplot(as.data.frame(pp),aes(.focal,.zeta))+
 plot(m51)
 rs <- DHARMa::simulateResiduals(m51)
 plot(rs)
-DHARMa::plotResiduals(rs, form = data$Type, quantreg = T)
 
 # Quick look
 plot(allEffects(m51))
@@ -382,7 +430,7 @@ preds$lower_nbc <- BoxCoxInv(preds$lower, lambda = BoxCoxLambda(data$Syllables))
 preds$upper_nbc <- BoxCoxInv(preds$upper, lambda = BoxCoxLambda(data$Syllables))
 
 # Plot
-tiff("syllable_length_by_type.tiff", units="in", width=3, height=5, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/syllable_length_by_type.tiff", units="in", width=3, height=5, res=300)
 ggplot() +
   geom_pointrange(data = preds, aes(x = Type, y = fit_nbc, ymin = lower_nbc, ymax = upper_nbc)) +
   geom_errorbar(data = preds, aes(x = Type, y = fit_nbc, ymin = lower_nbc, ymax = upper_nbc), width = .05) +
@@ -416,6 +464,15 @@ ggplot(data, aes(x = Type, y = Allomorphy)) +
 m11 <- glmer(Allomorphy ~ Type + (1 + Type|Language), data, family = "binomial")
 m11
 
+# Fixed Effects:
+#   (Intercept)        TypeS  
+# -2.9254       0.5192 
+
+# Random effects:
+#   Groups   Name        Std.Dev. Corr 
+# Language (Intercept) 2.211         
+# TypeS       1.344    -0.76
+
 # Alternative model and comparison
 m12 <- glmer(Allomorphy ~ 1 + (1 + Type|Language), data, family = "binomial")
 anova(m11, m12)
@@ -427,13 +484,12 @@ anova(m11, m12)
 ci <- confint(m11, method="Wald")
 ci
 
-pp <- profile(m11)
-ggplot(as.data.frame(pp),aes(.focal,.zeta))+
-  geom_point()+geom_line()+
-  facet_wrap(~.par,scale="free_x")+
-  geom_hline(yintercept=0,colour="gray")+
-  geom_hline(yintercept=c(-1.96,1.96),linetype=2,
-             colour="gray")
+# 2.5 %    97.5 %
+#   .sig01              NA        NA
+# .sig02              NA        NA
+# .sig03              NA        NA
+# (Intercept) -4.1563742 -1.694469
+# TypeS       -0.5761887  1.614506
 
 # Quick look
 plot(allEffects(m11))
@@ -442,7 +498,6 @@ plot(allEffects(m11))
 plot(m11)
 rs <- DHARMa::simulateResiduals(m11)
 plot(rs)
-DHARMa::plotResiduals(rs, form = data$Type, quantreg = T)
 
 # Generate predictions for plotting
 preds <- as.data.frame(predictorEffect("Type", m11))
@@ -451,7 +506,7 @@ preds <- as.data.frame(predictorEffect("Type", m11))
 rpreds <- as.data.frame(ggpredict(m11, c("Type", "Language"), type = "random"))
 
 # Plot
-tiff("allomorphy_by_type.tiff", units="in", width=3, height=5, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/allomorphy_by_type.tiff", units="in", width=3, height=5, res=300)
 ggplot() +
   geom_pointrange(data = preds, aes(x = Type, y = fit, ymin = lower, ymax = upper)) +
   geom_errorbar(data = preds, aes(x = Type, y = fit, ymin = lower, ymax = upper), width = .05) +
@@ -479,7 +534,7 @@ mean(allomorphs$Allomorphs[allomorphs$Type == 'S'])
 # 2.340136
 
 # Plot Allomorphs by Type
-tiff("allomorphs_hist_by_type.tiff", units="in", width=4, height=3, res=300)
+tiff("/Users/pgr/Documents/Projects/Affix_phonology/affix_phonology/Images/allomorphs_hist_by_type.tiff", units="in", width=4, height=3, res=300)
 ggplot(allomorphs, aes(x = Allomorphs, group = Type, color = Type)) +
   geom_histogram(aes(y = after_stat(count / sum(count))), fill = "grey30", bins = 9,
                  data = ~ subset(., Type %in% c("P"))) +
